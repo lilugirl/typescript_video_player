910a1043006d0039e9df5f6955f6a9e895847d5d
lilu <lilugirl2005@126.com>
2020-06-23 23:18:32 +0800

    实现视频列表结构

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
-import a from "./a";
-import './a.css';
+
+import './main.css';
 
 
-console.log(a);
+let listItem = document.querySelectorAll('#list li');
+
+for (let i = 0; i < listItem.length; i++) {
+  listItem[i].addEventListener('click', function () {
+    let url = this.dataset.url;
+    let title = this.dataset.title;
+    console.log(url, title);
+  })
+}
