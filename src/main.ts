6545d658fbe160228170ad520f5c8cd6a5616c3d
lilu <lilugirl2005@126.com>
2020-06-24 00:03:16 +0800

    组件框架搭建

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 
 import './main.css';
+import popup from './components/popup/popup';
 
 
 let listItem = document.querySelectorAll('#list li');
@@ -8,6 +9,13 @@ for (let i = 0; i < listItem.length; i++) {
   listItem[i].addEventListener('click', function () {
     let url = this.dataset.url;
     let title = this.dataset.title;
-    console.log(url, title);
+    //console.log(url, title);
+
+
+
   })
 }
+
+popup({});
+popup({});
+popup({});
